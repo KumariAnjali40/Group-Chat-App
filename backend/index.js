459344450493efaa3514a0257661b73cce65cac3
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-01-27 03:06:28 +0530

    github

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 const express=require('express');
+const mongoose=require('mongoose');
 const socketIO=require('socket.io');
-const {connection}=require('./db')
+const jwt=require('jsonwebtoken');
+const {UserModel}=require('./schema/user.model')
+const {connection}=require('./db');
 const http=require('http');
 const {userRouter}=require('./routes/user.routes');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const {auth}=require('./middleware/auth.middleware');
 const cors=require('cors');
 
@@ -21,6 +25,63 @@ const port=process.env.PORT||3000;
 app.get('/',(req,res)=>{
   res.send("Hello world");
 })
+
+//github start
+app.get('/auth/github',async(req,res)=>{
+  const {code}=req.query;
+  console.log(code);
+ const token_response= await fetch("https://github.com/login/oauth/access_token",{
+      method:"POST",
+      headers:{
+        Accept:"application/json",
+      "content-type":"application/json"
+      },
+      body:JSON.stringify({
+          client_id:"1c5423b8349237604119",
+          client_secret:"6ac4a4f083d43e9bee62a3dfcfd8416d14dc5143",
+          code:code
+      })
+  })
+  .then((res)=>res.json())
+  console.log(token_response);
+  const access_token=token_response.access_token
+  res.send("code is:"+code);
+
+  const githubUserResponse =await fetch("https://api.github.com/user",{
+      headers:{
+          Authorization: `Bearer ${access_token}`
+      }
+  })
+  const githubUser = await githubUserResponse.json();
+  console.log(githubUser);
+
+  // const userId = String(githubUser.id);
+  const user = await UserModel.findOneAndUpdate(
+      { name: githubUser.login },
+      {
+          $set: {
+              name: githubUser.login,
+              image:githubUser.avatar_url,
+          },
+      },
+      { upsert: true, new: true }
+  );
+
+  console.log(user);
+
+  const token = jwt.sign({ userID: user._id }, "Anjali");
+
+  // res.json({ token });
+  console.log(token);
+  // res.send(token);
+  
+ 
+});
+
+
+
+
+//github end
 
 
 //connection start
