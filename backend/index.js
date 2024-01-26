7853ce1ae3f5ed27118a4971252c0e428c6c6b77
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-01-26 20:32:06 +0530

    chat app done

--- backend/index.js
+++ backend/index.js
@@ -24,24 +24,26 @@ app.get('/',(req,res)=>{
 
 
 //connection start
-let user={};
+let users={};
 
 io.on("connection",(socket)=>{
   console.log(socket.id);
   socket.on("new-user-joined",(username)=>{
-    user[socket.id]=username;
-    console.log(user);
+    users[socket.id]=username;
+    console.log(users);
     socket.broadcast.emit('user-connected',username);
+    io.emit("user-list",users);
   });
-
-  socket.on("new_user",()=>{
-    console.log("hii");
-
-    socket.emit("user_details","hello")
-})
 
   socket.on("disconnect",()=>{
     console.log("client has disconnected")
+    socket.broadcast.emit('user-disconnected',user=users[socket.id]);
+    delete users[socket.id];
+    io.emit("user-list",users);
+  });
+
+  socket.on('message',(data)=>{
+    socket.broadcast.emit('message',{user:data.user,msg:data.msg});
   })
 })
 
